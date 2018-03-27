0cf0da58243ee9d1b4cdf8fae450b5bcb563ab2a
stefanshaffer <37416180+stefanshaffer@users.noreply.github.com>
2018-03-26 21:33:16 -0400

    Add files via upload

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -1,14 +1,9 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,7 +12,50 @@ for(var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
 	// add click listeners to squares
 	squares[i].addEventListener("click", function(){
-		alert("clicked a square");
-	})
+		// grab color of clicked square
+		var clickedColor = (this.style.backgroundColor);
+		//compare color to pickedColor
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct!";
+			changeColors(pickedColor);
+		} else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again!";
+		}
+	});
 }
 
+
+function changeColors(color) {
+	// loop through all squares 
+	for (var i = 0; i < colors.length; i++) {
+	//change each color to match given color
+		squares[i].style.backgroundColor = color;
+	}};
+
+	function pickColor() {
+		// pick a color square
+		var random = Math.floor(Math.random() * colors.length);
+		return colors[random];
+	}
+
+
+
+function generateRandomColors(num) {
+// make an array
+	var arr = []
+for (var i = 0; i < num; i++) {	
+	//generate color and push into array
+	arr.push(randomColor());
+	}
+	return arr
+}
+
+
+	function randomColor() {
+		// generate a random color
+	red = Math.floor(Math.random() * 256);
+	green = Math.floor(Math.random() * 256);
+	blue = Math.floor(Math.random() * 256);
+	return "rgb("+ red + ", " + green + ", " + blue + ")";
+	}
